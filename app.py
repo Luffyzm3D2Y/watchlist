95f8d29bd52fa466e4c347b6e2b9a29548895dd2
Jasper You <18879455172@163.com>
2023-02-13 15:06:00 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 from flask import Flask, render_template
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import  escape
+import click
+import os
+
 app=Flask(__name__)
+#设置mysql数据库URI，告诉SQLAlchemy数据库的连接地址
+"""
+SQLAlchemy indicates the source of an Engine as a URI combined with optional keyword arguments to specify options for the Engine. 
+The form of the URI is:
+dialect+driver://username:password@host:port/database
+"""
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:yzm@localhost:3306/dbOne'
+#dbOne是通过mysql为该项目建立的数据库
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #关闭对模型修改的监控
+#在扩展类实例化之前修改加载配置
+
+db=SQLAlchemy(app) #import扩展类并初始化扩展，传入实例app
+
 #__name__是一个全局变量，随着脚本执行或模块的引入而改变
+class User(db.Model):
+    """
+    创建数据库模型
+    模型类和字段
+    模型类的声明要继承db.Model
+    类字段要实例化db.Column,传入的参数为字段类型等，还有：
+    primary_key:bool
+    nullable:bool
+    index:bool
+    unique:bool
+    default:bool
+    表中的记录以模型类的实例表示
+    """
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
 
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+
+
+
+
+
+
+
+
 #@app.route('/')
 #def index():
 #    return render_template('index.html',name=name,movies=movies)
@@ -38,13 +69,16 @@ def index():
 
     f'<h1>Hello Flask!!!</h1><img src="http://helloflask.com/totoro.gif">'
 
+
+    user=User.query.first()
+    movies=Movie.query.all()
     """
     render_template()用于渲染模板，传入参数有：
     模板文件名
     通过关键字参数传入模板内部使用的变量
     render_template()调用后执行模板里所有的Jinja2语句并返回渲染好的模板内容
     """
-    return render_template('index.html',name=name,movies=movies)
+    return render_template('index.html',user=user,movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
     return f'User:{escape(name)}'
@@ -69,3 +103,35 @@ def test_url_for():
 #flask默认把程序存储在名为app.py或wsgi.py的文件中
 #如果要使用其他名称需要设置 系统环境变量 FLASK_APP 为要启动的程序名
 
+@app.cli.command()
+def forge():
+    """
+    自定义命令函数，用于一次性执行，相当于编写批处理
+    :return:
+    """
+    db.create_all()
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie=Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo("Done.")
+
+if __name__=="__main__":
+    db.drop_all()
+    db.create_all()
